c9d5f382a648b10d3c6ac446aa9c88bd65f36641
Agnesa Tsvetanova <agnesa.tsvetanova1995@gmail.com>
2022-04-05 11:40:56 +0300

    new

---
+++ ConverterUSDToBGN.java
@@ -0,0 +1,7 @@
+public class ConverterUSDToBGN {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+        double usd = double.parseDouble(Scnner.nextLine);
+        System.out.println(usd*1.79549);
+    }
+}
