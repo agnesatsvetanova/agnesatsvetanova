fe7f45d6ba257e2b5bf08128c9bd6a48124f5538
agnesatsvetanova <agnesa.tsvetanova1995@gmail.com>
2022-07-29 12:56:11 +0300

    remove file

--- ConverterUSDToBGN.java
+++
@@ -1,7 +0,0 @@
-public class ConverterUSDToBGN {
-    public static void main(String[] args) {
-        Scanner scanner = new scanner(System.in);
-        double usd = double.parseDouble(Scnner.nextLine);
-        System.out.println(usd*1.79549);
-    }
-}
